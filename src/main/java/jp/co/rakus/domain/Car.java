c363e0c0360426699227ccc379644eee05f3a2ec
uemurakento <kento.uemura@rakus.co.jp>
2018-10-30 12:22:49 +0900

    テストクラスの追加

--- src/main/java/jp/co/rakus/domain/Car.java
+++ src/main/java/jp/co/rakus/domain/Car.java
@@ -2,6 +2,15 @@ package jp.co.rakus.domain;
 
 public class Car {
 	private Integer speed;
+	private String bodyColor;
+
+	public String getBodyColor() {
+		return bodyColor;
+	}
+
+	public void setBodyColor(String bodyColor) {
+		this.bodyColor = bodyColor;
+	}
 
 	public Integer getSpeed() {
 		return speed;
