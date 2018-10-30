161e4b7c526abfb0afd773f1e4bbcd0d5fe59663
uemurakento <kento.uemura@rakus.co.jp>
2018-10-30 11:25:20 +0900

    新規にCarクラスを作成およびスタートプログラムにコメントを出力するように修正

---
+++ src/main/java/jp/co/rakus/domain/Car.java
@@ -0,0 +1,13 @@
+package jp.co.rakus.domain;
+
+public class Car {
+	private Integer speed;
+
+	public Integer getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(Integer speed) {
+		this.speed = speed;
+	}
+}
